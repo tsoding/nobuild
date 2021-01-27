56103f9f63dd604e5a98785e599f4af17014e033
rexim <reximkut@gmail.com>
2021-01-27 21:43:03 +0700

    Add TODO(#3)

--- nobuild.h
+++ nobuild.h
@@ -220,7 +220,7 @@ void mkdirs_impl(int ignore, ...)
 
 #define MKDIRS(...) mkdirs_impl(69, __VA_ARGS__, NULL)
 
-// TODO: there is no way to remove a folder
+// TODO(#3): there is no way to remove a folder
 // TODO: there is no way to remove a file
 
 const char *concat_impl(int ignore, ...)
