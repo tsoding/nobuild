5ffe5cc99f5228766151d2f487a3e2a2c9e357d7
rexim <reximkut@gmail.com>
2021-01-29 23:53:44 +0700

    (#3) Add recursive file traversal to the examples

--- examples/file.c
+++ examples/file.c
@@ -4,10 +4,23 @@
 #define DEMO(expr)                              \
     INFO("    "#expr" == %d", expr)
 
+void print_file_recursively(const char *path)
+{
+    FOREACH_FILE_IN_DIR(file, path, {
+        const char *child = PATH(path, file);
+        INFO("    %s", child);
+        if (*file != '.' && IS_DIR(child)) {
+            print_file_recursively(child);
+        }
+    });
+}
+
 int main(int argc, char *argv[])
 {
     DEMO(IS_DIR("./nobuild.c"));
     DEMO(IS_DIR("./examples"));
     DEMO(IS_DIR("./file_that_does_not_exist"));
+    INFO("Recursively traversing the file system");
+    print_file_recursively(".");
     return 0;
 }
