0f3bf419e54ef6bde366ef9d7fdf3c5ef8e76d52
rexim <reximkut@gmail.com>
2021-02-05 02:40:46 +0700

    Enable piping on Windows

--- examples/pipe.c
+++ examples/pipe.c
@@ -3,13 +3,9 @@
 
 int main(void)
 {
-#if _WIN32
-    WARN("Piping is not implemented on Windows yet");
-#else
     CHAIN(IN(PATH("examples", "pipe.c")),
           CHAIN_CMD(PATH("tools", "rot13")),
           CHAIN_CMD(PATH("tools", "hex")));
-#endif
 
     return 0;
 }
