306952d6c964daf133ec0e5bc040cfbd2b2e85f9
rexim <reximkut@gmail.com>
2021-03-23 17:56:53 +0700

    Make chain_run_sync() sync again just to test MSVC build

--- examples/pipe.c
+++ examples/pipe.c
@@ -4,11 +4,13 @@
 int main(void)
 {
     CHAIN(IN(PATH("examples", "pipe.c")),
-          CHAIN_CMD(PATH("tools", "rot13")),
-          CHAIN_CMD(PATH("tools", "hex"))
+          CHAIN_CMD(PATH("tools", "rot13"))//,
+          // CHAIN_CMD(PATH("tools", "hex")),
           // OUT("output.txt")
           );
-    //CMD(PATH("tools", "cat"), "output.txt");
+    // CMD(PATH("tools", "cat"), "output.txt");
+
+    // sleep(1);
 
     return 0;
 }
