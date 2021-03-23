903f3c6937e4b075c4000d9987c1d1621bd1ffb5
rexim <reximkut@gmail.com>
2021-03-23 19:11:17 +0700

    Redirect the output of pipe to a file

--- examples/pipe.c
+++ examples/pipe.c
@@ -3,14 +3,13 @@
 
 int main(void)
 {
-    INFO("------------------------------");
     CHAIN(IN(PATH("examples", "pipe.c")),
           CHAIN_CMD(PATH("tools", "rot13")),
-          CHAIN_CMD(PATH("tools", "hex"))
-          // OUT("output.txt")
-         );
-    INFO("------------------------------");
-    // CMD(PATH("tools", "cat"), "output.txt");
+          CHAIN_CMD(PATH("tools", "hex")),
+          OUT("output.txt"));
+    INFO("------------------------------------------------------------");
+    CMD(PATH("tools", "cat"), "output.txt");
+    INFO("------------------------------------------------------------");
 
 #ifdef _WIN32
     Sleep(1000);
